aeb5d8e5de7186a967824e87c34d147de059340f
hector guevara <hectorguevara@hectors-MacBook-Pro.local>
2017-12-03 17:54:11 -0600

    git ignore push

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -1,26 +1,32 @@
 const express= require('express');
 const router = express.Router();
 const User = require('../models/users.js');
+const bcrypt = require('bcrypt');
 
 router.get('/new', (req, res) => {
   res.render('sessions/new.ejs');
 });
+router.get('/profile', (req, res) => {
+  res.render('sessions/profile.ejs');
+});
 
-
-router.post('/', async (req, res) => {
+router.post('/login', async (req, res) => {
   const foundUser = await User.findOne({ username: req.body.username});
-  if ( req.body.password == foundUser.password) {
+  if ( bcrypt.compareSync(req.body.password, foundUser.password)) {
     req.session.currentuser = foundUser;
-    res.redirect('/');
+    res.redirect('/profile.ejs');
   } else {
     res.send('wrong password');
   }
 });
 
+
+
 router.delete('/', (req, res) => {
     req.session.destroy();
     res.redirect('/');
 });
+
 
 
 
--- controllers/users.js
+++ controllers/users.js
@@ -1,15 +1,54 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/users.js');
-cosnt bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
 
 router.get('/new', (req, res) => {
   res.render('users/new.ejs');
 });
 
-router.post('/', async (req, res) => {
-  await User.create(req.body);
-  res.redirect('/');
+// router.post('/register', async (req, res) => {
+//   req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
+//   await User.create(req.body);
+//   res.redirect('/');
+//
+// });
+// Register
+router.post('/register', async (req, res, next) => {
+
+  // Create Hash Password
+  const password = req.body.password;
+  const passwordHash = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+  const username = req.body.username;
+
+  // Create an object for our db entry
+  const userDbEntry = {};
+  userDbEntry.username = username;
+  userDbEntry.password = passwordHash;
+  console.log(userDbEntry);
+
+
+   try {
+     const users = await User.find({username: req.body.username});
+     if (users.length == 0) {
+    const user = await User.create(userDbEntry);
+    console.log(user);
+    req.session.username = user.username;
+    req.session.logged = true;
+    res.redirect('/');
+
+  } else {
+    // req.session.message = "'Username taken'";
+    res.send('Username taken');
+  }
+     console.log(user);
+   } catch(err) {
+     res.send('Failed to create user')
+     console.log('Register Error: ', err);
+   }
 });
+
+
+
 
 module.exports = router;
